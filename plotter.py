817c3945f491dd80fd47d9822c58309e19f503c6
duncanhong <chisheng@couchbase.com>
2013-01-11 23:25:51 -0800

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -33,7 +33,7 @@ def get_metric(db, metric, host_ip, bucket_name, query_params, start_time, end_t
                         "reducer": "avg",
                         "from": start_time,
                         "to": end_time,
-                        "f": ["mc-host", "mc-bucket"],
+                        "f": ["/mc-host", "/mc-bucket"],
                         "fv": [host_ip, bucket_name]
                       }
     else:
