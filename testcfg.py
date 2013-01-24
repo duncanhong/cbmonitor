04d4c67f437b391544588392b48079ffec5d63d5
duncanhong <chisheng@couchbase.com>
2013-01-23 17:31:10 -0800

    Update testcfg.py

--- testcfg.py
+++ testcfg.py
@@ -1,25 +1,8 @@
-#TODO: allow workers to pull this from cache
-
-RABBITMQ_IP = '127.0.0.1'
-OBJECT_CACHE_IP = "127.0.0.1"
-OBJECT_CACHE_PORT = "11911"
-SERIESLY_IP = ''
-COUCHBASE_IP = '127.0.0.1'
-COUCHBASE_PORT = '9000'
+SERIESLY_IP = '10.6.2.58'
+COUCHBASE_IP = '10.6.2.37'
+COUCHBASE_PORT = '8091'
 COUCHBASE_USER = "Administrator"
-COUCHBASE_PWD = "asdasd"
+COUCHBASE_PWD = "password"
 SSH_USER = "root"
-SSH_PASSWORD = "password"
-WORKERS = ['127.0.0.1']
-# valid configs ["kv","query","admin","stats"] or ["all"]
-WORKER_CONFIGS = ["kv"]
-CB_CLUSTER_TAG = "default"
+SSH_PASSWORD = "couchbase"
 ATOP_LOG_FILE = "/tmp/atop-node.log"
-LOGDIR="logs"  # relative to current dir
-
-#Backup Config
-ENABLE_BACKUPS = False
-BACKUP_DIR = "/tmp/backup"
-BACKUP_NODE_IP = "127.0.0.1"
-BACKUP_NODE_SSH_USER = "root"
-BACKUP_NODE_SSH_PWD = "password"
